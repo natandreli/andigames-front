ec2cb5048c59edad58de2e73a64c964040674b09
Natalia Andrea García Ríos <natalia@MacBook-Air-de-Natalia.local>
2023-11-07 13:38:11 -0500

    Landing page almost done

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -8,10 +8,31 @@ const lexend = Lexend({ subsets: ['latin'], weights: [400, 500, 600, 700] })
 
 export default function Home() {
 
-  const router = useRouter();
+    const router = useRouter();
 
-  return (
-    <div>
-    </div>
-  )
+    return (
+        <div>
+            <nav className="flex items-center justify-between p-6 pl-10 pr-10">
+                <div className="flex items-center flex-shrink-0 text-white mr-6">
+                    <span className={`font-bold text-sl tracking-tight ${lexend.className}`}>
+                        <button
+                            onClick={() => router.push('/')}
+                        >
+                            ANDIGAMES
+                        </button>
+                    </span>
+                </div>
+            </nav>
+            <div className='container mx-auto px-10 pt-5 text-center'>
+                <h1 className={`font-bold text-5xl text-center text-white leading-tight ${lexend.className}`}>Juega, califica y conecta: Tu plataforma para compartir tu experiencia gamer</h1>
+                <h2 className={`text-base mt-5 text-center text-white leading-tight`}>¡Bienvenido a ANDIGAMES! Tu punto de partida para sumergirte en el mundo de los videojuegos, calificar tus experiencias y unirte a una comunidad de jugadores apasionados.</h2>
+                <button
+                    className="m-auto mt-10 bg-blue-800 hover:bg-blue-600 text-white px-4 py-4 rounded-full"
+                    onClick={() => router.push('/signup')}
+                >
+                    Comenzar en ANDIGAMES
+                </button>
+            </div>
+        </div>
+    )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -15,34 +15,40 @@ export default function Home() {
       <nav className="flex items-center justify-between p-6 pl-10 pr-10">
         <div className="flex items-center flex-shrink-0 text-white mr-6">
           <span className={`font-bold text-sl tracking-tight ${lexend.className}`}>
-            <button>ANDIGAMES</button>
+            <button
+              onClick={() => router.push('/')}
+            >
+              ANDIGAMES
+            </button>
           </span>
         </div>
         <div className="w-full flex items-center">
           <div className={`text-sm ml-auto`}>
-            <button 
+            <button
               className="text-white hover:text-blue-400 py-2 px-4 rounded-full mr-2"
-              onClick = {() =>
-                router.push('/login')}>
-              Inicia sesión</button>
-            <button 
+              onClick={() => router.push('/login')}
+            >
+              Inicia sesión
+            </button>
+            <button
               className="bg-white hover:bg-blue-800 text-neutral-700 hover:text-white py-2 px-4 rounded-full"
-              onClick = {() =>
-                router.push('/signup')}>
-              Regístrate</button>
+              onClick={() => router.push('/signup')}
+            >
+              Regístrate
+            </button>
           </div>
         </div>
       </nav>
       <div className='container mx-auto px-10 pt-5 text-center'>
-          <h1 className={`font-bold text-5xl text-center text-white leading-tight ${lexend.className}`}>Juega, califica y conecta: Tu plataforma para compartir tu experiencia gamer</h1>
-          <h2 className={`text-base mt-5 text-center text-white leading-tight`}>¡Bienvenido a ANDIGAMES! Tu punto de partida para sumergirte en el mundo de los videojuegos, calificar tus experiencias y unirte a una comunidad de jugadores apasionados.</h2>
-          <button 
-            className="m-auto mt-10 bg-blue-800 hover:bg-blue-600 text-white px-4 py-4 rounded-full"
-            onClick = {() =>
-              router.push('/signup')}>
-            Comenzar en ANDIGAMES
-          </button>
-        </div>
+        <h1 className={`font-bold text-5xl text-center text-white leading-tight ${lexend.className}`}>Juega, califica y conecta: Tu plataforma para compartir tu experiencia gamer</h1>
+        <h2 className={`text-base mt-5 text-center text-white leading-tight`}>¡Bienvenido a ANDIGAMES! Tu punto de partida para sumergirte en el mundo de los videojuegos, calificar tus experiencias y unirte a una comunidad de jugadores apasionados.</h2>
+        <button
+          className="m-auto mt-10 bg-blue-800 hover:bg-blue-600 text-white px-4 py-4 rounded-full"
+          onClick={() => router.push('/signup')}
+        >
+          Comenzar en ANDIGAMES
+        </button>
       </div>
+    </div>
   )
 }
