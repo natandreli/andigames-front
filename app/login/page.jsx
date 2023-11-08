0c379921ed6b895c3c15ffdac2e3a59278152f75
natandreli <natalia.garcia9@udea.edu.co>
2023-11-07 23:15:55 -0500

    signup page almost done, improvements in the responsive design and others

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { Lexend, Manrope } from 'next/font/google';
 import { useRouter } from 'next/navigation';
-import Image from 'next/image';
+import { Lexend, Manrope } from 'next/font/google';
 
 const lexend = Lexend({ subsets: ['latin'], weights: [400, 500, 600, 700] })
 
@@ -18,7 +17,7 @@ export default function Home() {
         <div className='h-screen bg-neutral-900 bg-opacity-70'>
             <nav className="items-center justify-between p-7 pl-10 pr-10 w-full">
                 <div className="flex items-center flex-shrink-0 text-white mr-6">
-                    <span className={`font-bold text-sl tracking-tight pt-1 ${lexend.className}`}>
+                    <span className={`font-bold sm:text-lg text-xs tracking-tight pt-1 ${lexend.className}`}>
                         <button
                             onClick={() => router.push('/')}
                         >
@@ -29,29 +28,29 @@ export default function Home() {
             </nav>
             <div className='flex flex-col items-center justify-center'>
                 <div className='container mx-auto px-10 text-center'>
-                    <h1 className={`font-bold text-5xl text-center text-white leading-tight ${lexend.className}`}>
+                    <h1 className={`font-bold text-3xl sm:text-5xl text-center text-white leading-tight ${lexend.className}`}>
                         ¡Qué bueno volver a verte!
                     </h1>
-                    <h2 className={`font-semibold text-2xl mt-5 text-center text-neutral-400 leading-tight ${lexend.className}`}>
+                    <h2 className={`font-semibold text-lg sm:text-2xl mt-5 text-center text-neutral-400 leading-tight ${lexend.className}`}>
                         ¡Ingresa para jugar, calificar y conectar!
                     </h2>
                 </div>
                 <div className="w-full max-w-md items-center justify-center m-10 bg-neutral-800 rounded p-10">
                     <form className="">
                         <div className="mb-4">
-                            <h2 className={`font-bold text-3xl text-center text-white ${lexend.className}`}>
+                            <h2 className={`font-bold text-xl sm:text-3xl text-center text-white ${lexend.className}`}>
                                 Inicia sesión
                             </h2>
                         </div>
                         <div className="mb-4">
-                            <label className="block text-neutral-400 text-sm font-bold mb-2" for="username">
-                                Usuario
+                            <label className="block text-neutral-400 text-sm font-bold mb-2">
+                                Usuario <span className="text-red-500">*</span>
                             </label>
                             <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Ingresa tu nombre de usuario"></input>
                         </div>
                         <div className="mb-9">
-                            <label className="block text-neutral-400 text-sm font-bold mb-2" for="password">
-                                Contraseña
+                            <label className="block text-neutral-400 text-sm font-bold mb-2">
+                                Contraseña <span className="text-red-500">*</span>
                             </label>
                             <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Ingresa tu contraseña"></input>
                         </div>
@@ -67,11 +66,11 @@ export default function Home() {
                     </form>
                     <div className="">
                         <button
-                            className="w-full hover:text-blue-400 text-blue-600 font-base px-4 rounded"
+                            className="w-full hover:text-blue-400 text-blue-600 font-sm px-4 rounded"
                             onClick={() =>
                                 router.push('../signup')}
                         >
-                            ¿Nuevo en GAMETAG? Regístrate ahora
+                            ¿Nuevo en ANDIGAMES? Regístrate ahora
                         </button>
                     </div>
                 </div>
--- app/page.jsx
+++ app/page.jsx
@@ -14,7 +14,7 @@ export default function Home() {
     <div>
       <nav className="flex items-center justify-between p-6 pl-10 pr-10 w-full">
         <div className="flex items-center flex-shrink-0 text-white mr-6">
-          <span className={`font-bold text-sl tracking-tight ${lexend.className}`}>
+          <span className={`font-bold sm:text-lg text-xs tracking-tight ${lexend.className}`}>
             <button
               onClick={() => router.push('/')}
             >
@@ -23,7 +23,7 @@ export default function Home() {
           </span>
         </div>
         <div className="w-full flex items-center">
-          <div className={`text-sm ml-auto`}>
+          <div className={`sm:text-sm ml-auto text-xs`}>
             <button
               className="text-white hover:text-blue-400 py-2 px-4 rounded-full mr-2"
               onClick={() => router.push('/login')}
@@ -40,10 +40,10 @@ export default function Home() {
         </div>
       </nav>
       <div className='container mx-auto px-10 pt-5 text-center'>
-        <h1 className={`font-bold text-5xl text-center text-white leading-tight ${lexend.className}`}>Juega, califica y conecta: Tu plataforma para compartir tu experiencia gamer</h1>
+        <h1 className={`font-bold text-3xl sm:text-5xl text-center text-white leading-tight ${lexend.className}`}>Juega, califica y conecta: Tu plataforma para compartir tu experiencia gamer</h1>
         <h2 className={`text-base mt-5 text-center text-white leading-tight`}>¡Bienvenido a ANDIGAMES! Tu punto de partida para sumergirte en el mundo de los videojuegos, calificar tus experiencias y unirte a una comunidad de jugadores apasionados.</h2>
         <button
-          className="m-auto mt-10 bg-blue-800 hover:bg-blue-600 text-white px-4 py-4 rounded-full"
+          className="m-auto mt-10 bg-blue-800 hover:bg-blue-600 text-white px-4 py-3 rounded-full"
           onClick={() => router.push('/signup')}
         >
           Comenzar en ANDIGAMES
