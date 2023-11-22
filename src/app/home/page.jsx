f383fd12067a0853f2278cb19124587862289b07
natandreli <natalia.garcia9@udea.edu.co>
2023-11-22 18:43:47 -0500

    Change colors of search input and make it responsive

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -202,7 +202,7 @@ export default function Home() {
             </div>
             <div className="flex flex-wrap items-center sm:py-2 px-10 w-full sm:mt-0 mt-4">
                 {!showWishList &&
-                    <div className="block text-[8px] lg:text-xs text-neutral-400">
+                    <div className="block text-xs xl:text-sm text-neutral-400">
                         <ul className="flex flex-wrap">
                             <li className="mr-4 mb-2">
                                 <button
@@ -278,21 +278,21 @@ export default function Home() {
                             </li>
                         </ul>
                     </div>}
-                <div className="sm:ml-auto flex flex-wrap items-center text-[8px] lg:text-xs mb-2">
+                <div className="lg:ml-auto mt-4 lg:mt-0 flex flex-wrap items-center text-[8px] lg:text-xs mb-2">
                     <div className="mr-4">
                         <div className="pt-1 sm:pt-2 relative mb-2">
-                            <input className="text-sm border h-6 lg:h-10 border-[#005EC1] border-opacity-50 bg-neutral-900 bg-opacity-80 text-neutral-400 px-4 rounded-md focus:outline-none w-80" name="search" placeholder="Buscar" />
-                            <button type="submit" className="absolute right-0 top-0 mt-3 sm:mt-5 mr-4">
-                                <svg className="h-2 w-2 lg:h-4 lg:w-4 fill-[#005EC1] hover:fill-opacity-50" xmlns="http://www.w3.org/2000/svg" version="1.1" id="Capa_1" viewBox="0 0 56.966 56.966">
+                            <input className="border h-9 sm:h-10 border-[#9170DC] border-opacity-50 bg-[#9170DC] placeholder-[#9170DC] bg-opacity-10 text-neutral-400 px-4 rounded-md focus:outline-none w-[280px] sm:w-[550px] md:w-[400px] lg:w-[220px] xl:w-[300px] 2xl:w-[500px] text-xs xl:text-sm " name="search" placeholder="Buscar juego" />
+                            <button type="submit" className="absolute right-0 top-0 mt-3.5 sm:mt-5 mr-4">
+                                <svg className="h-4 :w-4 fill-[#9170DC] hover:fill-opacity-50" xmlns="http://www.w3.org/2000/svg" version="1.1" id="Capa_1" viewBox="0 0 56.966 56.966">
                                     <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
                                 </svg>
                             </button>
                         </div>
                     </div>
                     {/* <span className="text-neutral-400 mr-4">Número de juegos: {gamesToShow.length}</span> */}
-                    <div className="mb-4 flex flex-col">
-                        <span className="text-neutral-400 mb-0.5">Género</span>
-                        <select className="text-sm text-neutral-400 bg-neutral-900 px-2 py-1 border border-neutral-500 rounded-md h-10">
+                    <div className="mb-5 flex flex-col">
+                        <span className="text-neutral-400 mb-0.5 text-xs xl:text-sm ml-1 mt-1 md:mt-0">Género</span>
+                        <select className="text-neutral-400 bg-neutral-900 px-2 py-1 border border-neutral-500 rounded-md h-9 sm:h-10 w-[280px] sm:w-[275px] md:w-[260px] lg:w-[125px] xl:w-[200px] text-xs xl:text-sm">
                             <option value="" defaultValue>Todos</option>
                             {genres.map((genre) => (
                                 <option key={genre} value={genre}>{genre}</option>
