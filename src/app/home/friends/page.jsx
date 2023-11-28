3bd20808d34b3f49643ef43197ee61d7c6fb1e41
natandreli <natalia.garcia9@udea.edu.co>
2023-11-28 17:06:28 -0500

    search user

--- src/app/home/friends/page.jsx
+++ src/app/home/friends/page.jsx
@@ -100,35 +100,42 @@ export default function Home() {
             <div className="px-10 xl:px-20 flex flex-wrap items-center text-sm lg:text-base">
                 <div className="pt-1 sm:pt-2 relative w-full mt-4 sm:mt-0">
                     <form onSubmit={handleSearchUser}>
-                    <button
-                        type="submit"
-                        className="flex items-center justify-center h-9 sm:h-11 w-11 bg-[#A61145] hover:bg-[#D92B68] hover:bg-opacity-30 bg-opacity-20 cursor-pointer absolute left-0 top-0 pl-1 mt-1 sm:mt-2 rounded-l-md"
-                    >
-                        <svg className="h-4 w-4 fill-[#A61145]" xmlns="http://www.w3.org/2000/svg" version="1.1" id="Capa_1" viewBox="0 0 56.966 56.966">
-                            <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
-                        </svg>
-                    </button>
-                    <input
-                        className={`pl-14 text-sm sm:text-base border h-9 sm:h-11 ${badSearch ? `border-red-500` : `border-[#A61145]`} border-opacity-50 border-2 bg-neutral-900 placeholder-neutral-400 bg-opacity-5 text-neutral-400 px-4 rounded-md focus:outline-none w-full`}
-                        name="search"
-                        placeholder="Buscar amigo"
-                        value={searchValue}
-                        onChange={(e) => {
-                            const value = e.target.value;
-                            if (value === '') {
-                                setShowUsers(true); // Asegurar que se muestren las predicciones
-                                setSearchValue('');
-                                setIsSearching(false);
-                                setFoundUser([]);
-                                setAlreadySearch(false);
-                                setBadSearch(false);
-                                setShouldUpdateUsers(true);
-                            } else {
-                                setSearchValue(value);
-                                setShowUsers(false); // No mostrar predicciones mientras se busca
-                            }
-                        }}
-                    />
+                        <button
+                            type="submit"
+                            className="flex items-center justify-center h-9 sm:h-11 w-11 bg-[#A61145] hover:bg-[#D92B68] hover:bg-opacity-30 bg-opacity-20 cursor-pointer absolute left-0 top-0 pl-1 mt-1 sm:mt-2 rounded-l-md"
+                        >
+                            <svg className="h-4 w-4 fill-[#A61145]" xmlns="http://www.w3.org/2000/svg" version="1.1" id="Capa_1" viewBox="0 0 56.966 56.966">
+                                <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+                            </svg>
+                        </button>
+                        <input
+                            className={`pl-14 text-sm sm:text-base border h-9 sm:h-11 ${badSearch ? `border-red-500` : `border-[#A61145]`} border-opacity-50 border-2 bg-neutral-900 placeholder-neutral-400 bg-opacity-5 text-neutral-400 px-4 rounded-md focus:outline-none w-full`}
+                            name="search"
+                            placeholder="Buscar amigo"
+                            value={searchValue}
+                            onChange={(e) => {
+                                const value = e.target.value;
+                                if (value === '') {
+                                    setShowUsers(true); // Asegurar que se muestren las predicciones
+                                    setSearchValue('');
+                                    setIsSearching(false);
+                                    setFoundUser([]);
+                                    setAlreadySearch(false);
+                                    setBadSearch(false);
+                                    setShouldUpdateUsers(true);
+                                } else {
+                                    setSearchValue(value);
+                                    setShowUsers(false); // No mostrar predicciones mientras se busca
+                                }
+                            }}
+                        />
+                        {badSearch &&
+                            <div className="pt-3 text-sm">
+                                <span className="text-red-500">
+                                    Por favor, llena los campos requeridos.
+                                </span>
+                            </div>
+                        }
                     </form>
                 </div>
             </div>
